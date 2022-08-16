a3f2c0f35faf316677dc0d3d087a8832f894a8b0
Greg Ross <greg.ross@networldsports.com>
2022-08-16 08:36:33 +0100

    Initial Commit

---
+++ TestingThings/TestingThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingThingsApp.swift
+//  TestingThings
+//
+//  Created by Greg Ross on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
