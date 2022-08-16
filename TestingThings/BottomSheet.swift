a196624a12024039aecf518465650a10462e606d
Greg Ross <greg.ross@networldsports.com>
2022-08-16 12:55:39 +0100

    first commit

---
+++ TestingThings/BottomSheet.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSheet.swift
+//  TestingThings
+//
+//  Created by Greg Ross on 16/08/2022.
+//
+
+import SwiftUI
+
+struct BottomSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSheet()
+    }
+}
---
+++ TestingThings/StarRatingMask.swift
@@ -0,0 +1,20 @@
+//
+//  StarRatingMask.swift
+//  TestingThings
+//
+//  Created by Greg Ross on 16/08/2022.
+//
+
+import SwiftUI
+
+struct StarRatingMask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StarRatingMask_Previews: PreviewProvider {
+    static var previews: some View {
+        StarRatingMask()
+    }
+}
