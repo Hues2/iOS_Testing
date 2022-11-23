ae54baee8dc80b4110ccb743cbaf50912a54ce5b
Greg Ross <greg.ross@networldsports.com>
2022-11-23 09:23:09 +0000

    - Added an app delegate to handle the register for push notifications and firebase - Remote Firebase push notifications can now be sent

--- TestingThings/TestingThingsApp.swift
+++ TestingThings/TestingThingsApp.swift
@@ -6,12 +6,89 @@
 //
 
 import SwiftUI
+import Firebase
+import UserNotifications
+
+
 
 @main
 struct TestingThingsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
+    FirebaseApp.configure() /// --> Configure Firebase
+      
+      Messaging.messaging().delegate = self /// --> Adding the messaging delegate
+      
+      UNUserNotificationCenter.current().delegate = self
+      
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in }) /// --> Asking the user for permission to send notifications
+      
+      application.registerForRemoteNotifications()
+
+    return true
+  }
+    
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey]{
+            print("\n Message ID: \(messageID) \n")
+        }
+        print("\n \(userInfo) \n")
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+}
+
+
+extension AppDelegate: MessagingDelegate{
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        let deviceToken: [String: String] = ["token" : fcmToken ?? ""]
+        print("\n Device token: \(deviceToken) \n")
+    }
+    
+}
+
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        if let messageID = userInfo[gcmMessageIDKey]{
+            print("\n Message ID: \(messageID) \n")
+        }
+        
+        print("\n \(userInfo) \n")
+        
+        
+        completionHandler([[.banner, .badge, .sound]])
+    }
+    
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+}
+
